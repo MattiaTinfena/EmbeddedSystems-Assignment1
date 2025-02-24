1fc036d2fed1c3561232a9c2c641999a2ff09a59
MattiaTinfena <mattia.tinfena@gmail.com>
2025-02-24 12:36:55 +0100

    Ex-2

--- Assignment1.X/main.c
+++ Assignment1.X/main.c
@@ -2,17 +2,23 @@
  * File:   main.c
  * Author: mattia
  *
- * Created on February 23, 2025, 11:17 AM
+ * Created on February 24, 2025, 9:43 AM
  */
-
 
 #include "xc.h"
 
-
-int main(void) {
-    TRISA =  0x0000;
-    while(1){
-        LATA = 0X0001;
-    }
-    return 0;
-}
+ int main(void) {
+     ANSELA = ANSELB = ANSELC = ANSELD = ANSELE = ANSELG = 0x0000;
+     
+     TRISA = 0x0000;
+     TRISE = 0xFFFF;
+     
+     while(1){
+         if(!PORTEbits.RE8){
+             LATA = 0x0001;
+         } else{
+             LATA = 0x0000;
+         }
+     }
+     return 0;
+ }
