0f97adebbb062c322c30ab33145c99243d49deb2
MattiaTinfena <mattia.tinfena@gmail.com>
2025-02-25 11:17:16 +0100

    Ex-3

--- Assignment1.X/main.c
+++ Assignment1.X/main.c
@@ -1,24 +1,38 @@
 /*
  * File:   main.c
- * Author: mattia
+ * Author: Elisa
  *
  * Created on February 24, 2025, 9:43 AM
  */
 
-#include "xc.h"
 
- int main(void) {
-     ANSELA = ANSELB = ANSELC = ANSELD = ANSELE = ANSELG = 0x0000;
-     
-     TRISA = 0x0000;
-     TRISE = 0xFFFF;
-     
-     while(1){
-         if(!PORTEbits.RE8){
-             LATA = 0x0001;
-         } else{
-             LATA = 0x0000;
-         }
-     }
-     return 0;
- }
+#include "xc.h"
+#include "stdbool.h"
+
+int main(void) {
+    ANSELA = ANSELB = ANSELC = ANSELD = ANSELE = ANSELG = 0x0000;
+    
+    TRISA = 0x0000;
+    TRISE = 0xFFFF;
+    
+    bool toggle = false;
+    int state = PORTEbits.RE8;
+    int old_state = state;
+    
+    while(1){
+        
+        state = PORTEbits.RE8;
+        
+        if(PORTEbits.RE8 && state != old_state){
+            toggle = !toggle;
+        }   
+        if(toggle){
+            LATA = 0x0001;
+        } else {
+            LATA = 0x0000;
+        }
+        old_state = state;
+    }
+    
+    return 0;
+}
